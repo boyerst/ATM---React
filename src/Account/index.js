3a321592677a53603f66d5dfc24d2e97d58fa077
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-23 18:44:14 -0500

    fixed zero balance code

--- src/Account/index.js
+++ src/Account/index.js
@@ -12,17 +12,13 @@ class Account extends Component {
   }
 
   handleClick = (event) => {
-    console.log(event.target.value, "This is event.target.value")
-    console.log(event.target, "This is event.target")
-    
-
     const currentInput = this.myRef.current.value
     console.log(currentInput)
 
 
     if(event.target.value === 'Deposit'){
       this.setState({balance: currentInput})
-    } else if(this.state.balance >= currentInput) {
+    } else if (this.state.balance >= currentInput) {
       this.setState({balance: this.state.balance - currentInput})
     }
   }
@@ -33,12 +29,18 @@ class Account extends Component {
 
   render() {
     console.log(this.myRef, "This is myRef")
-    console.log(this.balance)
+
+    if(this.state.balance === 0) {
+      this.className = 'zero'
+    } else {
+      this.className = 'balance'
+    }
+    
   
     return (
       <div className="account">
         <h2>{this.props.name}</h2>
-        <div className="balance">${this.state.balance}</div>
+        <div className={this.className}>{this.state.balance}</div>
         <input type="text" 
                placeholder="enter an amount" 
                ref={this.myRef} />
@@ -56,45 +58,5 @@ export default Account;
 //7. if current balance in zero, add another class name to that particular div
   // if statement 
 
-//a way in react to interact w elements on the DOM (ie w buttons, forms, etc)
 
 
-//this.amount
-//this.amount.balance
-//this.amount.value?
-
-
-// import React, { Component } from 'react';
-
-
-// class Account extends Component {
-
-//   constructor() {
-//     super()
-//       this.state = {
-//         balance: 0
-//       }
-//   }
-
-//   handleSubmit = event => {
-//     this.state + this.input.value
-//   }
-
-//   render() {
-//     return (
-//       <div className="account">
-//         <h2>{this.props.name}</h2>
-//         <div className="balance">$0</div>
-//         <form onSubmit={this.handleSubmit} >
-//         <input type="text" 
-//                placeholder="enter an amount" 
-//                ref={(funds) => this.amount = funds} />
-//         </form>
-//         <input type="button" value="Deposit" />
-//         <input type="button" value="Withdraw" />
-//       </div>
-//     )
-//   }
-// }
-
-// export default Account;
